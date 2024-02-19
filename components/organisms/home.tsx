7f03b888c3226ca4e128026cb7bf2f2b580f6cb4
Patrice Diouf <trispa88@gmail.com>
2024-02-19 17:34:51 -0500

    fix: aligment

--- components/organisms/home.tsx
+++ components/organisms/home.tsx
@@ -16,7 +16,7 @@ function Home({ data }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl  p-5 lg:justify-evenly mx-auto items-center"
+      className="h-[100vh] relative flex flex-col text-center md:text-left md:flex-row max-w-7xl  p-5 lg:justify-evenly mx-auto items-center"
     >
       <motion.div
         initial={{
@@ -26,17 +26,39 @@ function Home({ data }: Props) {
         transition={{ duration: 1.2 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
+        className="md:flex flex-col"
       >
-        <h4 className="lg:text-6xl md:text-2xl text-base font-semibold mb-5 text-left">
+        <div className="lg:text-6xl md:text-2xl text-base font-semibold mb-5 text-left">
           {data.title[language.code]}
-        </h4>
-        <p className="md:text-base  text-sm text-justify">
+        </div>
+        <div className="md:text-base  text-sm text-justify">
           {data.description[language.code]}
-        </p>
-        <div className="hidden my-4 md:flex flex-col gap-4">
+        </div>
+        <div className=" my-4 flex-col gap-4">
           <Link
             href={"#contact"}
-            className="flex items-center gap-2 bg-gradient-to-r sm:w-40 capitalize font-bold from-primary-trid to-[#CFD02B] p-3 text-center  rounded-lg text-white"
+            className="md:flex  hidden items-center gap-2 bg-gradient-to-r sm:w-40 capitalize font-bold from-primary-trid to-[#CFD02B] p-3 text-center  rounded-lg text-white"
+          >
+            <h1 className="flex">{data.heroButtonLabel[language.code]}</h1>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </Link>
+
+          <Link
+            href={"#contact"}
+            className="gap-4 md:hidden  my-2  bottom-20  flex  items-center bg-gradient-to-r capitalize font-bold from-primary-trid to-[#CFD02B] p-3 text-center  rounded-lg text-white"
           >
             <h1 className="flex">{data.heroButtonLabel[language.code]}</h1>
             <svg
@@ -65,29 +87,8 @@ function Home({ data }: Props) {
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
         src={urlFor(data.heroImage).url()}
-        className="-mb-20 md:mb-0 flex-shrink-0 w-48 h-auto object-cover md:w-96 md:h-auto xl:w-[500px] xl:h-[600px]"
+        className="md:mb-0 flex-shrink-0 h-auto object-cover md:w-96 md:h-auto xl:w-[500px] xl:h-[600px]"
       />
-
-      <Link
-        href={"#contact"}
-        className="gap-4 md:hidden w-[90%] my-2 absolute bottom-20  flex  items-center bg-gradient-to-r capitalize font-bold from-primary-trid to-[#CFD02B] p-3 text-center  rounded-lg text-white"
-      >
-        <h1 className="flex">{data.heroButtonLabel[language.code]}</h1>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
-          />
-        </svg>
-      </Link>
     </motion.div>
   );
 }
