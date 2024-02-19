ae7678690b5dd8f8de8c7b654f5d0b6aea30704e
Patrice Diouf <trispa88@gmail.com>
2024-02-18 21:18:24 -0500

    fix: change the replyTo email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -30,7 +30,7 @@ exports.sendEmail = functions.https.onRequest((req, res) => {
       from: `"TRID" <${CONTACT_EMAIL}>`,
       to: CONTACT_EMAIL,
       sender: CONTACT_EMAIL_AUTH,
-      replyTo: CONTACT_EMAIL_AUTH,
+      replyTo: CONTACT_EMAIL,
       subject: "Contact from TRID Website",
       html,
     };
