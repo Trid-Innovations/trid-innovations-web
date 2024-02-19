299a32506e1bb7849fd1fc3f43a7cb1eb8220a85
Patrice Diouf <trispa88@gmail.com>
2024-02-18 21:06:07 -0500

    add trid logo into email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,7 +21,7 @@ exports.sendEmail = functions.https.onRequest((req, res) => {
     if (number) {
       html = `${html}<br> Phone number: ${number}`;
     }
-    html = `${html}<br> Message: ${message}`;
+    html = `${html}<br> Message: <br> ${message}`;
 
     let signature = `<br> <a href="www.tridinnovations.com">TRID Innovations Team</a>`;
     signature = `${signature}<br><br><img src="https://travelwithziggy.com/wp-content/uploads/2022/10/T.W.Z-logotype-1-150x150-1.png"/>`;
