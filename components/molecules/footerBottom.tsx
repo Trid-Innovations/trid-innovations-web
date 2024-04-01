028174296cffa7c4e9910134a7a39bb355e4ecdf
Patrice Diouf <trispa88@gmail.com>
2024-04-01 18:07:17 -0400

    remove unused footer element

--- components/molecules/footerBottom.tsx
+++ components/molecules/footerBottom.tsx
@@ -7,10 +7,10 @@ function FooterBottom({ data }: { data: FooterData }) {
   return (
     <div className="flex items-center  md:justify-between flex-wrap text-xs  mb-5 gap-3">
       <div>{data.copyRights[language.code]}</div>
-      <div className="flex flex-wrap uppercase gap-2">
+      {/* <div className="flex flex-wrap uppercase gap-2">
         <div>{data.termsOfUse[language.code]}</div>
         <div>{data.privacyPolicy[language.code]}</div>
-      </div>
+      </div> */}
     </div>
   );
 }
